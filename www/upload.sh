8dabb7fc0c2c457b597baeda3dc51c5c1e5ec001
Rave Strapon <ravestrapon@gmail.com>
2017-11-27 09:32:33 -0800

    Add the website data
    This adds in the website data and the upload script.
    The script requires ssh keys to communicate with the server,
    but otherwise all the data is here.
    
    Signed-off-by: Rave Strapon <ravestrapon@gmail.com>

---
+++ www/upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RAVE_STRAPON_LOCAL_DIR="."
+RAVE_STRAPON_SSH_KEY_DIR="~/.ssh/ravestrapon/"
+USERNAME="cmooney3_ravestrapon"
+SSH_SERVER="ssh.phx.nearlyfreespeech.net"
+RAVE_STRAPON_REMOTE_DIR="/home/public"
+
+rsync -vazh --delete --delete-excluded \
+  -e "ssh -i \"${RAVE_STRAPON_SSH_KEY_DIR}/id_rsa\"" \
+  --exclude "ssh_keys*" --exclude "upload.sh" \
+  --exclude "*.swp" --exclude "*.swo" \
+  "${RAVE_STRAPON_LOCAL_DIR}/" \
+  "${USERNAME}@${SSH_SERVER}:${RAVE_STRAPON_REMOTE_DIR}/"
