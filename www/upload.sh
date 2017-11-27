cecb39914d401aab72781b7b8a9854b50ff87534
Rave Strapon <ravestrapon@gmail.com>
2017-11-27 11:55:09 -0800

    Remove uneeded argument
    There was an unnessicary exception added to the rsync command since
    we're not storing the ssh keys in with src anymore (now that the code is
    on github)
    
    Signed-off-by: Rave Strapon <ravestrapon@gmail.com>

--- www/upload.sh
+++ www/upload.sh
@@ -8,7 +8,6 @@ RAVE_STRAPON_REMOTE_DIR="/home/public"
 
 rsync -vazh --delete --delete-excluded \
   -e "ssh -i \"${RAVE_STRAPON_SSH_KEY_DIR}/id_rsa\"" \
-  --exclude "ssh_keys*" --exclude "upload.sh" \
-  --exclude "*.swp" --exclude "*.swo" \
+  --exclude "upload.sh" --exclude "*.swp" --exclude "*.swo" \
   "${RAVE_STRAPON_LOCAL_DIR}/" \
   "${USERNAME}@${SSH_SERVER}:${RAVE_STRAPON_REMOTE_DIR}/"
