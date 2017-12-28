0e2cc0ee15d3dca128bea5cd11fc9e0270397375
Rave Strapon <ravestrapon@gmail.com>
2017-12-28 13:35:19 -0800

    Use internal ADC reference for battery monitor
    Using the internal 2.56V reference voltage instead
    of Vcc for measuring the battery voltages.  This makes it
    much more accurate, but requires the use of external
    voltage dividers to bring the signals into range, so the
    SW has to compensate.  This patch also includes precisely
    computed scalers for each of the probes on my battery
    monitor -- obviously those numbers have to change with
    different resistor values.
    
    Signed-off-by: Rave Strapon <ravestrapon@gmail.com>

--- etc/battery_monitor/battery_monitor.ino
+++ etc/battery_monitor/battery_monitor.ino
@@ -1,36 +1,36 @@
 #define LED_PIN 3
 #define MON_PIN_0 0
-#define MON_PIN_0_SCALER 1.0  // Pin 0 has no voltage divider
-#define MON_PIN_1 1
-#define MON_PIN_1_SCALER 2.0  // Pin 1 has a 1/2 voltage divider
+#define MON_PIN_0_SCALER 0.3590205257  // Pin 0 has a voltage divider
+#define MON_PIN_1 3
+#define MON_PIN_1_SCALER 0.3518185092  // Pin 1 has a voltage divider
 
-#define V_MAX 5.04
-#define V_MIN 0.0
 #define ADC_STEPS 1024.0
+#define VCC 2.56 
 
 #define DELAY_MS 200
 
 void setup() {
-  Serial.begin(9600);
+  // Use the internal 1.1V reference instead of Vcc for the
+  // ADC readings.  This is more stable and produces better
+  // accuracy, although the voltages need to be divided down
+  // to under 1.1V now.
+  analogReference(INTERNAL); 
 
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
+  // Turn on serial communication so we can report the readings
+  Serial.begin(9600);
 }
 
-double ConvertToVoltage(int adc_reading, double scaler) {
-  return scaler * adc_reading * ((V_MAX - V_MIN) / ADC_STEPS) + V_MIN;
+double GetVoltage(int pin, double scaler) {
+  int adc_reading = analogRead(pin);
+  return ((double)adc_reading / ADC_STEPS) * VCC / scaler;
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-
-  Serial.print(ConvertToVoltage(analogRead(MON_PIN_0),
-                                MON_PIN_0_SCALER));
+  Serial.print(GetVoltage(MON_PIN_0, MON_PIN_0_SCALER));
   Serial.print("V\t");
-  Serial.print(ConvertToVoltage(analogRead(MON_PIN_1),
-                                MON_PIN_1_SCALER));
-  Serial.print("V\n");
+  Serial.print(GetVoltage(MON_PIN_1, MON_PIN_1_SCALER));
+  Serial.print("V\t");
+  Serial.print("\n");
 
-  digitalWrite(LED_PIN, LOW);
   delay(DELAY_MS);
 }
