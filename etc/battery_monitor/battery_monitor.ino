b298c7d81e2c88b4b7638fc3ce020dc6231e7cb5
Rave Strapon <ravestrapon@gmail.com>
2017-12-28 13:35:24 -0800

    Speed up sampling and change pin #
    Signed-off-by: Rave Strapon <ravestrapon@gmail.com>

--- etc/battery_monitor/battery_monitor.ino
+++ etc/battery_monitor/battery_monitor.ino
@@ -1,13 +1,13 @@
 #define LED_PIN 3
 #define MON_PIN_0 0
 #define MON_PIN_0_SCALER 0.3590205257  // Pin 0 has a voltage divider
-#define MON_PIN_1 3
-#define MON_PIN_1_SCALER 0.3518185092  // Pin 1 has a voltage divider
+#define MON_PIN_3 3
+#define MON_PIN_3_SCALER 0.3518185092  // Pin 1 has a voltage divider
 
 #define ADC_STEPS 1024.0
 #define VCC 2.56 
 
-#define DELAY_MS 200
+#define DELAY_MS 100
 
 void setup() {
   // Use the internal 1.1V reference instead of Vcc for the
@@ -28,7 +28,7 @@ double GetVoltage(int pin, double scaler) {
 void loop() {
   Serial.print(GetVoltage(MON_PIN_0, MON_PIN_0_SCALER));
   Serial.print("V\t");
-  Serial.print(GetVoltage(MON_PIN_1, MON_PIN_1_SCALER));
+  Serial.print(GetVoltage(MON_PIN_3, MON_PIN_3_SCALER));
   Serial.print("V\t");
   Serial.print("\n");
 
