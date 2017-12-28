f69c32f16086a5411783cb0d4e262770a571f521
Rave Strapon <ravestrapon@gmail.com>
2017-12-28 13:35:41 -0800

    Add ATMega16 testing code
    This is a simple Hello World program to blink an
    LED and set up the fuses on an ATMega16.  This proves
    that the programmer and the circuit board are all working :D
    
    Signed-off-by: Rave Strapon <ravestrapon@gmail.com>

---
+++ avr/main.c
@@ -0,0 +1,19 @@
+#include<avr/io.h>
+#include <util/delay.h>
+
+#define DELAY_MS 1000
+ 
+int main()
+{
+  DDRD = 0x80;  // Configure pin D0 as output
+ 
+  while(1) {
+    PORTD = 0xFF; // Turn on LED
+    _delay_ms(DELAY_MS);
+
+    PORTD = 0x00; // Turn off LED
+    _delay_ms(DELAY_MS);
+  }
+
+  return 0;
+}
